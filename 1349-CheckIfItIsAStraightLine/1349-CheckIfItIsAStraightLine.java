63daf49b3c7d0e44ffcd9251751a2e2beecc3c15
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:06:47 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.java
@@ -0,0 +1,25 @@
+// Last updated: 09/07/2026, 15:06:48
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x1 = coordinates[0][0];
+        int y1 = coordinates[0][1];
+        int x2 = coordinates[1][0];
+        int y2 = coordinates[1][1];
+
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        for (int i = 2; i < coordinates.length; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+
+            // Check cross product for collinearity
+            if (dx * (y - y1) != dy * (x - x1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+        
+    }
