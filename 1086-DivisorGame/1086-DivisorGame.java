4cabcb8c2270b87681b14c92be3f25bdcbac47da
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:07:02 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 15:07:02
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
