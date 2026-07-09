3192f28e82b66366afb7fe794ea8f9f1d0fda3c2
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:10:00 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,12 @@
+# Last updated: 09/07/2026, 15:10:01
+class Solution(object):
+    def multiply(self, num1, num2):
+        n1 = 0
+        for i in num1:
+            n1 = n1*10 + (ord(i) - 48)
+
+        n2 = 0
+        for j in num2:
+            n2 = n2*10 + (ord(j) - 48)   
+        
+        return str(n1*n2)
