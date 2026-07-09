7c9b5ec65a7d7bab2a29ed4e8b8c3bc926918016
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:08:01 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 15:08:01
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+}
