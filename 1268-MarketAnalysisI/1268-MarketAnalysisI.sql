dbad5d170e76853a6c849029f3bd03c13c8db643
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:06:50 +0530

    Create 1268-MarketAnalysisI

---
+++ 1268-MarketAnalysisI/1268-MarketAnalysisI.sql
@@ -0,0 +1,9 @@
+-- Last updated: 09/07/2026, 15:06:51
+SELECT u.user_id AS buyer_id, join_date, 
+IFNULL(COUNT(order_date), 0) AS orders_in_2019 
+FROM Users as u
+LEFT JOIN
+Orders as o
+ON u.user_id = o.buyer_id
+AND YEAR(order_date) = '2019'
+GROUP BY u.user_id
