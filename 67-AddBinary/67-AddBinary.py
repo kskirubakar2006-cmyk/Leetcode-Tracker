210105abcec52ceaee24eb1b59ed9fcc643972b7
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:09:29 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.py
@@ -0,0 +1,20 @@
+# Last updated: 09/07/2026, 15:09:29
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        res = []
+        
+        idxA, idxB = len(a) - 1, len(b) - 1
+        
+        while idxA >= 0 or idxB >= 0 or carry == 1:
+            if idxA >= 0:
+                carry += int(a[idxA])
+                idxA -= 1            
+            if idxB >= 0:
+                carry += int(b[idxB])
+                idxB -= 1            
+
+            res.append(str(carry % 2))
+            carry = carry // 2
+            
+        return "".join(res[::-1])
