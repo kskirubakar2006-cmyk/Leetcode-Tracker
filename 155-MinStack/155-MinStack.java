cd0ce42a4f7dc69aa145f1ece90b25cf6557eaba
kskirubakar2006-cmyk <kskirubakar2006@gmail.com>
2026-07-09 15:08:52 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,35 @@
+// Last updated: 09/07/2026, 15:08:52
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (!minStack.isEmpty()) {
+            val = Math.min(minStack.peek(), val);
+        }
+
+        minStack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
